76dcca24369ea2941c78901abdf7508ad9c3c87b
Alain Bangoula <alain.bangoula@come-n-see.fr>
2013-01-30 18:14:38 +0100

    Update Parsley/ServerBundle/Controller/ValidationController.php

--- Parsley/ServerBundle/Controller/ValidationController.php
+++ Parsley/ServerBundle/Controller/ValidationController.php
@@ -11,7 +11,7 @@ class ValidationController extends Controller
     public function validationAction($form_service_name, $field_to_compare)
     {
         
-        $list($field, $errors) = $this->get('parsley_server_form_generator')->validateField($form_service_name, $field_to_compare);
+        list($field, $errors) = $this->get('parsley_server_form_generator')->validateField($form_service_name, $field_to_compare);
 
         if(count($errors) == 0){
             $response = new Response(1);
