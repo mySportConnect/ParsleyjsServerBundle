958fbeaf457621bfd6646be4b6e4910695d97527
Alain Bangoula <alain.bangoula@come-n-see.fr>
2013-01-30 17:12:54 +0000

    putting validation for a field in a service

--- Parsley/ServerBundle/Controller/ValidationController.php
+++ Parsley/ServerBundle/Controller/ValidationController.php
@@ -10,21 +10,8 @@ class ValidationController extends Controller
 {
     public function validationAction($form_service_name, $field_to_compare)
     {
-        $form_type = $this->get('form.registry')->getType($form_service_name)->getInnerType();
-        $tmp_form = $this->get('form.factory')->create($form_type);
-
-        $config = $this->get('service_container')->getParameter('parsley_server');
-
-        $form = $this->get('form.factory')->createNamed($tmp_form->getName(), $form_type, null, array('validation_groups' =>  $config['validations'][$form_service_name]));
-
-        $form->bind($this->get('request'));
-
-        $field = $form->get($field_to_compare);
-        $errors = array();
-
-        foreach($field->getErrors() as $idx => $error){
-                $errors[] =  $this->get('translator')->trans($error->getMessage(), array(), 'validators',  $this->get('request')->getLocale());
-        }
+        
+        $list($field, $errors) = $this->get('parsley_server_form_generator')->validateField($form_service_name, $field_to_compare);
 
         if(count($errors) == 0){
             $response = new Response(1);
---
+++ Parsley/ServerBundle/Form/Generator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Parsley\ServerBundle\Form;
+
+
+class Generator 
+{
+    private $container;
+
+    public __function($container)
+    {
+        $this->container = $container;
+    }
+
+    public function validateField($form_service_name, $field_to_compare)
+    {
+        
+        $form_type = $this->get('form.registry')->getType($form_service_name)->getInnerType();
+        $tmp_form = $this->get('form.factory')->create($form_type);
+
+        $config = $this->get('service_container')->getParameter('parsley_server');
+
+        $form = $this->get('form.factory')->createNamed($tmp_form->getName(), $form_type, null, array('validation_groups' =>  $config['validations'][$form_service_name]));
+
+        $form->bind($this->get('request'));
+
+        $field = $form->get($field_to_compare);
+        $errors = array();
+
+        foreach($field->getErrors() as $idx => $error){
+                $errors[] =  $this->get('translator')->trans($error->getMessage(), array(), 'validators',  $this->get('request')->getLocale());
+        }
+
+        return array($field, $errors);
+
+    }
+}
