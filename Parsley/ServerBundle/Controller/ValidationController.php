328374be3b88acb468d357e97f1baf13738ec0c0
Remiii <repository@remibarbe.fr>
2013-02-10 12:28:50 +0000

    Add TryCatch method instead of error list.
    On branch master
    	modified:   Parsley/ServerBundle/Controller/ValidationController.php
    	modified:   Parsley/ServerBundle/Form/Generator.php

--- Parsley/ServerBundle/Controller/ValidationController.php
+++ Parsley/ServerBundle/Controller/ValidationController.php
@@ -11,27 +11,23 @@ class ValidationController extends Controller
     public function validationAction ( $form_service_name , $field_to_compare )
     {
 
-        list ( $field , $errors ) = $this -> get ( 'parsley_server_form_generator' ) -> validateField ( $form_service_name , $field_to_compare ) ;
-
         $response = new JsonResponse ( ) ;
 
-        if ( count ( $errors ) == 0 )
+        try
         {
+            $this -> get ( 'parsley_server_form_generator' ) -> validateField ( $form_service_name , $field_to_compare ) ;
             $response -> setData ( array (
                 'success' => ''
             ) ) ;
+
         }
-        else
+        catch (\Exception $e)
         {
-            $message = '' ;
-            foreach ( $errors as $error )
-            {
-                $message .= $error ;
-            }
             $response -> setData ( array (
-                'error' => $message
+                'error' => $e -> getMessage ( )
             ) ) ;
         }
+
         return $response ;
 
     }
--- Parsley/ServerBundle/Form/Generator.php
+++ Parsley/ServerBundle/Form/Generator.php
@@ -1,49 +1,45 @@
 <?php
 
-namespace Parsley\ServerBundle\Form;
+namespace Parsley\ServerBundle\Form ;
 
 class UndefinedValidationException extends \Exception
 {
 }
 
-class Generator 
+class Generator
 {
-    private $container;
+    private $container ;
 
-    public function __construct($container)
+    public function __construct ( $container )
     {
-        $this->container = $container;
+        $this -> container = $container ;
     }
 
-    public function validateField($form_service_name, $field_to_compare)
+    public function validateField ( $form_service_name , $field_to_compare )
     {
-        
-        $form_type = $this->container->get('form.registry')->getType($form_service_name)->getInnerType();
-        $tmp_form = $this->container->get('form.factory')->create($form_type);
 
-        $config = $this->container->getParameter('parsley_server');
-        
-        try {
+        $form_type = $this -> container -> get ( 'form.registry' ) -> getType ( $form_service_name ) -> getInnerType ( ) ;
+        $tmp_form = $this -> container -> get ( 'form.factory' ) -> create ( $form_type ) ;
 
-            $form = $this->container->get('form.factory')->createNamed($tmp_form->getName(), $form_type, null, array('validation_groups' =>  $config['validations'][$form_service_name]['group']));
-        
-        } catch (\Exception $e)
-        
+        $config = $this -> container -> getParameter ( 'parsley_server' ) ;
+
+        try
         {
-            throw new UndefinedValidationException("Please define a form validation for this form.");
+            $form = $this -> container -> get ( 'form.factory' ) -> createNamed ( $tmp_form -> getName ( ) , $form_type , null , array ( 'validation_groups' => $config [ 'validations' ] [ $form_service_name ] [ 'group' ] ) ) ;
         }
-        
-
-        $form->bind($this->container->get('request'));
-
-        $field = $form->get($field_to_compare);
-        $errors = array();
-
-        foreach($field->getErrors() as $idx => $error){
-                $errors[] =  $this->container->get('translator')->trans($error->getMessage(), array(), $config['validations'][$form_service_name]['translation_domain'],  $this->container->get('request')->getLocale());
+        catch ( \Exception $e )
+        {
+            throw new UndefinedValidationException ( 'Please define a form validation for this form.' ) ;
         }
 
-        return array($field, $errors);
+        $form -> bind ( $this -> container -> get ( 'request' ) ) ;
+        $field = $form -> get ( $field_to_compare ) ;
+        $errors = array ( ) ;
+
+        foreach ( $field -> getErrors ( ) as $idx => $error )
+        {
+                throw new \Exception ( $this -> container -> get ( 'translator' ) -> trans ( $error -> getMessage ( ) , array ( ) , $config [ 'validations' ] [ $form_service_name ] [ 'translation_domain' ] , $this -> container -> get ( 'request' ) -> getLocale ( ) ) ) ;
+        }
 
     }
 }
